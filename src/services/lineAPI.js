0d37d4e703963d43e833158d17ea8489899a19e9
Taninchot <taninchot.art@gmail.com>
2022-01-14 16:41:50 +0700

    [UPDATE] Add variable as messageToSend in handleBeacon and add default for switch case

--- src/services/lineAPI.js
+++ src/services/lineAPI.js
@@ -1,36 +1,45 @@
 const { clientLine } = require("../configs/line")
 const { fullDate } = require("../helpers/date")
-const { getProfile } = require("../helpers/fetchLineProfile")
+const { getUserProfile } = require("../helpers/fetchLineProfile")
 const database = require("./database")
 
 const handleEvent = async (events) => {
   const event = events[0]
-  console.log(event)
   switch (event.type) {
     case "message":
-      if (event.source.type == "user") {
-        await sendMessage(event.source.userId, event.message.text)
-      }
+      await handleMessageEvent(event)
       break;
     case "beacon":
       await handleBeacon(event)
       break
+    default:
+      throw new Error(`At this time the server cannot support "${event.type}" event`)
   }
 }
 
 const sendMessage = async (id, text) => {
-  clientLine.pushMessage(id, { type: "text", text: text })
+  await clientLine.pushMessage(id, { type: "text", text: text })
+}
+
+const handleMessageEvent = async (event) => {
+  if (event.source.type == "user" && event.message.type == "text") {
+    await sendMessage(event.source.userId, event.message.text)
+  } else {
+    throw new Error(`At this time the server cannot support "${event.message.type}" with "${event.source.type}" event`)
+  }
 }
 
 const handleBeacon = async (event) => {
   const date = fullDate(Date.now())
-  const data = await database.getByDocument(date, event.source.userId)
-  const lineName = await getProfile(event.source.userId)
-  const sendTo = process.env.GROUP_ID || event.source.userId
+  const todayUserDBData = await database.getByDocument(date, event.source.userId)
 
-  if (!data) {
+  const user = await getUserProfile(event.source.userId)
+  const sendTo = process.env.GROUP_ID || event.source.userId
+  const messageToSend = `${user.displayName} ได้เข้ามา Office แล้ว`
+
+  if (!todayUserDBData) {
     await database.createDocument(date, event.source.userId)
-    await sendMessage(sendTo, `${lineName} ได้เข้ามา Office แล้ว`)
+    await sendMessage(sendTo, messageToSend)
   }
 }
 
