aa4de35e913c9af66a70274bd189e0890cd60d00
Taninchot <taninchot.art@gmail.com>
2022-01-14 10:12:52 +0700

    [UPDATE] Add send to group id env

--- src/services/lineAPI.js
+++ src/services/lineAPI.js
@@ -26,7 +26,7 @@ const handleBeacon = async (event) => {
   const date = fullDate(Date.now())
   const data = await database.getByDocument(date, event.source.userId)
   const lineName = await getProfile(event.source.userId)
-  const sendTo = event.source.userId
+  const sendTo = process.env.GROUP_ID || event.source.userId
 
   if (!data) {
     await database.createDocument(date, event.source.userId)
