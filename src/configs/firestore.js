68b75d5a496e4651c88173583851153a626831af
Taninchot <taninchot.art@gmail.com>
2022-01-14 11:29:37 +0700

    [UPDATE] Fix env and fix health status

--- src/configs/firestore.js
+++ src/configs/firestore.js
@@ -1,19 +1,13 @@
 const firestore = require('firebase-admin');
+const { cert } = require('firebase-admin/app');
 
 let certJSON = {
-  type: process.env.type,
-  project_id: process.env.project_id,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url
+  projectId: process.env.project_id,
+  clientEmail: process.env.client_email,
+  privateKey: process.env.private_key,
 }
 
-firestore.initializeApp(certJSON);
+firestore.initializeApp({ credential: cert(certJSON) })
 
 module.exports = firestore
 
--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -3,7 +3,10 @@ const { middleware } = require("../configs/line")
 const lineAPI = require("../services/lineAPI")
 
 router.post("/", middleware, async (req, res) => {
-  await lineAPI(req.body.events)
+  if (req.body.events.length > 0) {
+    await lineAPI(req.body.events)
+  }
+  return res.send(200)
 })
 
 module.exports = router
