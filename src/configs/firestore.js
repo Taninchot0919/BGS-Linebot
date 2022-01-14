355cec7042f2479a55f36fc97b69008a45e0e6e5
Taninchot <taninchot.art@gmail.com>
2022-01-14 11:51:54 +0700

    [UPDATE] private key using regular express

--- src/configs/firestore.js
+++ src/configs/firestore.js
@@ -1,13 +1,13 @@
-const firestore = require('firebase-admin');
+const firebase = require('firebase-admin');
 const { cert } = require('firebase-admin/app');
 
-let certJSON = {
+let certObj = {
   projectId: process.env.project_id,
   clientEmail: process.env.client_email,
-  privateKey: process.env.private_key,
+  privateKey: process.env.private_key.replace(/\\n/g, '\n'),
 }
 
-firestore.initializeApp({ credential: cert(certJSON) })
+firebase.initializeApp({ credential: cert(certObj) })
 
-module.exports = firestore
+module.exports = firebase
 
